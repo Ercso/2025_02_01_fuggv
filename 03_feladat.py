0f4d37d7c64f86f796b8df75cdaa2466f6ae20ae
Szalkay Erik <szalkay.erik@diak.szbi-pg.hu>
2025-02-01 09:18:19 +0100

    reaél

---
+++ 03_feladat.py
@@ -0,0 +1,17 @@
+"""3.1 Feladat
+Írj egy programot, amely tartalmaz egy 'harommal_oszthatok' nevű függvényt, amely a paraméterként átvett listában megvizsgálja, hogy hány darab hárommal osztható szám van, és ezzel az értékkel tér vissza! A program tartalmazza a függvény hívását is!"""
+
+def harommal_oszthatok(szamok):
+    db = 0
+    for szam in szamok:
+        if szam % 3 == 0:
+            db += 1
+    return db
+
+print(harommal_oszthatok([1, 12, 3, 4, 6]))
+
+"""3.2 Feladat
+Alakítsd át az előző programot úgy, hogy a felhasználó által megadott számokat tárolja el a program egy listában, és ezt értékelje ki a függvény! (Az adatbeolvasás addig tartson, míg a felhasználó negatív számot nem ad meg!)"""
+
+
+
